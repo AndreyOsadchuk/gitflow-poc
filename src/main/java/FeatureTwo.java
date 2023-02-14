367861f706e2953c40ca82b7e6a31437c6f371a5
AndriiOsadchuk <andriiosad4uk@gmail.com>
2023-02-14 17:07:39 +0200

    FeatureTwo hotfix

--- src/main/java/FeatureTwo.java
+++ src/main/java/FeatureTwo.java
@@ -1,5 +1,10 @@
 public class FeatureTwo {
     public static void main(String[] args) {
+        FeatureTwo featureTwo = new FeatureTwo();
+        featureTwo.bugFix();
+    }
+
+    private void bugFix(){
 
     }
 }
